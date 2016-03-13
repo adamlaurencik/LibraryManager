1c811ddd73c43117c2797a249bfa850fb66e6829
Adam <laurencik.a@gmail.com>
2016-03-13 20:28:43 +0100

    Úprava Junit testov pred implementáciou

---
+++ src/test/java/librarymanager/managers/impl/BorrowManagerImplTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package librarymanager.managers.impl;
+
+
+import org.junit.Before;
+
+/**
+ *
+ * @author Adam Laurenčík
+ */
+public class BorrowManagerImplTest {
+    
+
+    private BorrowManagerImpl manager;
+    
+    @Before
+    public void setUp() {
+        manager= new BorrowManagerImpl();
+    }
+    
+}
