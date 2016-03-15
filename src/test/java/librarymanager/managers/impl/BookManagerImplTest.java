97c70542a57bf1fd8459839e4642b5783a0695a8
Adam <laurencik.a@gmail.com>
2016-03-15 17:21:40 +0100

    deleteBorrowTest

--- src/test/java/librarymanager/managers/impl/BookManagerImplTest.java
+++ src/test/java/librarymanager/managers/impl/BookManagerImplTest.java
@@ -5,7 +5,12 @@
  */
 package librarymanager.managers.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import librarymanager.entities.Book;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,26 +20,53 @@ import org.junit.Test;
  * @author Adam Laurenčík
  */
 public class BookManagerImplTest {
-    
+
     private BookManagerImpl manager;
-    
+
     @Before
     public void setUp() {
         manager = new BookManagerImpl();
     }
-    
-    
+
     @Test
-    public void getAllBooks(){
-        
+    public void listAllBooks() {
         assertTrue(manager.listAllBooks().isEmpty());
+        Book book1 = new Book();
+        book1.setAuthor("Joshua Bloch");
+        book1.setName("Effective Java");
+        book1.setIsbn("978-0321356680");
+
+        Book book2 = new Book();
+        book2.setAuthor("Johnny Mecoch");
+        book2.setName("Skola tanca");
+        book2.setIsbn("238-8321565103");
+
+        manager.createBook(book1);
+        assertTrue(manager.listAllBooks().size() == 1);
+
+        manager.createBook(book2);
+        assertTrue(manager.listAllBooks().size() == 2);
+       
         
-        Book book = new Book();
-        book.setAuthor("Author");
-        book.setName("Testing book");
-        book.setAuthor("Author");
-        book.setAuthor("Author");
+        List<Book> expected = new ArrayList<>();
+        expected.add(book1);
+        expected.add(book2);
         
+        List<Book> result = manager.listAllBooks();
+        Collections.sort(expected, idComparator);
+        Collections.sort(result, idComparator);
+        assertEquals(expected,result);
         
+        manager.deleteBook(book1);
+        assertEquals(manager.listAllBooks().size(),2);
     }
+    
+    
+
+    private static Comparator<Book> idComparator = new Comparator<Book>() {
+        @Override
+        public int compare(Book o1, Book o2) {
+            return Long.compare(o1.getId(), o2.getId());
+        }
+    };
 }
