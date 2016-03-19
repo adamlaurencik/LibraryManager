9e23a84018a8ad716d4ae2ed495ed35d4e57856e
Adam <laurencik.a@gmail.com>
2016-03-19 15:06:58 +0100

    pridanie getAllBorrows

--- src/main/java/librarymanager/managers/impl/BorrowManagerImpl.java
+++ src/main/java/librarymanager/managers/impl/BorrowManagerImpl.java
@@ -56,5 +56,10 @@ public class BorrowManagerImpl implements BorrowManager {
     public boolean isBorrowed(Book book) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public List<Book> listAllBorrows() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
